e55a2f0479a2f2095d8480e1a7346b0ad99f5ef6
Erik <linarizkrp@mail.ru>
2021-11-16 08:56:03 +0300

    EventsAdder

---
+++ src/views/EventsAdder.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <v-container grid-list-xs>
+            <h1>{{ name }}</h1>
+            <h2></h2>
+        </v-container>
+    </div>
+</template>
+
+<style>
+
+</style>
