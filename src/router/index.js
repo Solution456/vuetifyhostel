13797dba458200306e417ee275cf9ce34af848e8
Кирилл Рыбаков <kotya.ribak@mail.ru>
2021-12-20 08:41:50 +0300

    working tableResident

--- src/router/index.js
+++ src/router/index.js
@@ -89,6 +89,7 @@ const routes = [
   {
     path: '/residentTable/:room',
     name: 'residentTable',
+    props: true,
     // route level code-splitting
     // this generates a separate chunk (residentTable.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
